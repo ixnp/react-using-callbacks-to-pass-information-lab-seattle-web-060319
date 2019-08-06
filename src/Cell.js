64a70819acad612dd9aeb0397f69cfb35380b550
ixnp <enprocopios@gmail.com>
2019-08-06 12:44:08 -0700

    finished

--- src/Cell.js
+++ src/Cell.js
@@ -8,10 +8,18 @@ export default class Cell extends Component {
       color: this.props.color
     }
   }
-  
+
+  handleClick = (e) => {
+    console.log('what',e)
+    let newColor = this.props.getcolor()
+    console.log(newColor)
+    this.setState({
+      color: newColor
+    })
+  }
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" style={{backgroundColor: this.state.color}} onClick={(e) => this.handleClick(e)}>
       </div>
     )
   }
--- src/Matrix.js
+++ src/Matrix.js
@@ -4,24 +4,39 @@ import Cell from './Cell.js'
 import ColorSelector from './ColorSelector.js'
 
 export default class Matrix extends Component {
-
+  //initalize the state with selected color
   constructor() {
     super()
+    this.state = {
+      selectedColor: '#FFF'
+    }
+  
+  }
+  setColor = str => {
+    this.setState({selectedColor: str})
   }
 
+    getColor = () => {
+      return this.state.selectedColor
+    }
+
+//create a getcolor funciton that gets the value of current states color
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    //this should call your getcolor
+    vals.map((val, idx) => <Cell key={idx} color={val} getcolor={this.getColor} />)
   )
 
   genMatrix = () => (
+    //this grabs the rows and passes it to genrow
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
-
+//need to pass ColorSelector the setcolor
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+    
+        <ColorSelector setColor={this.setColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
